ddb33e87d54e357227e58885dc831f8797061582
Dhruvi Desai <desaidhruvi345@gmail.com>
2025-05-23 21:07:02 +0530

    Emoji change

---
+++ emoji/script.js
@@ -0,0 +1,33 @@
+const  btn = document.querySelector('#emoji');
+
+const emoji = [
+   
+        "😀",
+        "😁",
+        "😂",
+        "😋",
+        "😎" ,
+        "😃",
+        "🥰",
+        "😅",
+        "😅",
+        "😍",
+        "🥲",
+        "🤔",
+        "🫡",
+        "🙄",
+        "🤐",
+        "😮",
+        "😴",
+        "😭",
+        "🥵",
+        "🥱",
+        "🥳",
+        "☠️",
+        "😱",
+
+];
+
+btn.addEventListener('mouseover', () => {
+    btn.innerHTML = emoji[Math.floor(Math.random() * emoji.length)];
+});
