d0aa7b548175bec9cac5f765b8c19d4d308aa4b6
Dhruvi Desai <desaidhruvi345@gmail.com>
2025-07-31 21:01:42 +0530

    Array

---
+++ JS IN-Built objects && arrays/index.js
@@ -0,0 +1,27 @@
+console.log('Hello Dhruvi');
+
+
+// string in primitve 
+
+let lastName ='desai';
+
+// string in object //
+
+let firstName = new String('dhruvi');
+
+ 
+let messsage = `this is my 
+first message`;
+
+console.log(messsage);
+
+// let words = messsage.split(' ');
+
+// console.log(words);
+
+
+// DATE CREATING //
+
+let date = new Date ();
+
+console.log(date);
